a553de644b5233e4ff13a503f3fbb101b31af8fe
Schwarze Katze <titanium22@126.com>
2023-01-04 06:53:34 +0800

    work progress on set cover

--- include/SetCover.h
+++ include/SetCover.h
@@ -7,4 +7,8 @@
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef Kernel::Point_2 Point_2;
 
-int GreedySetCovering(std::vector<Point_2> &V1,std::vector<Point_2> &coveredTarget);
+using std::vector;
+
+int GreedySetCovering(std::vector<Point_2>& V1, std::vector<Point_2>& coveredTarget, std::vector<Point_2>& V2);
+
+inline vector<int> SetCoveringProblem(vector<vector<bool>>& A, vector<int>& setsLabelsV, vector<int>& setsCardinalitiesV, vector<int>& setsCardinalitiesL);
--- src/SetCover.cpp
+++ src/SetCover.cpp
@@ -22,7 +22,84 @@
 // 	F. Gori, G. Folino, M.S.M. Jetten, E. Marchiori
 // 	"MTR: Taxonomic annotation of short metagenomic reads using clustering at multiple taxonomic ranks", Bioinformatics 2010.
 // 	doi = 10.1093/bioinformatics/btq649 
-int GreedySetCovering(std::vector<Point_2> &V1,std::vector<Point_2> &coveredTarget) {
+int GreedySetCovering(std::vector<Point_2>& V1, std::vector<Point_2>& coveredTarget, std::vector<Point_2>& V2) {
+    const int tlen = coveredTarget.size(), vlen = V1.size();
+    vector<vector<bool>> setCoverTable(tlen);
+    for (auto& tmp : setCoverTable) {
+        tmp.resize(vlen);
+    }
+    for (int i = 0;i < tlen;++i) {
+        for (int j = 0;j < vlen;++j) {
+            double d = sqrt((coveredTarget[i] - V1[j]).squared_length());
+            if (d <= TERRAConfig::problemParam.Radius) {
+                setCoverTable[i][j] = 1;
+            }
+            else {
+                setCoverTable[i][j] = 0;
+            }
+        }
+    }
+//  Building scp_table, without empty columns. Deleting the vertices which
+//  don't surround any waypoint
+    std::vector<int> deletedVertices;
+    vector<vector<bool>> setCoverTable_s(tlen);
+    for (int i = 0;i < vlen;++i) {
+        bool isCopy = false;
+        for (int j = 0;j < tlen;++j) {
+            if (setCoverTable[j][i]) {
+                isCopy = true;
+            }
+        }
+        if (isCopy) {
+            for (int j = 0;j < tlen;++j) {
+                setCoverTable_s[j].push_back(setCoverTable[j][i]);
+            }
+        }
+        else {
+            deletedVertices.push_back(i);
+        }
+    }
     
+    // Generate an adecuate labeling of the vertices, if it has been some vertice
+    // deleted so Lp is the complement of deleted_vertices
+    std::vector<int>Lp;
+    for (int i = 1;i <= vlen;++i) {
+        Lp.push_back(i);
+        if (std::find(deletedVertices.begin(), deletedVertices.end(), i) != deletedVertices.end()) {//find i in deleted vertices
+            Lp.back() = 0;
+        }
+    }
+
+    std::sort(Lp.rbegin(), Lp.rend());
+    while (Lp.back() == 0) {
+        Lp.pop_back();
+    }
+    std::reverse(Lp.begin(), Lp.end());
+
+    vector<int> setsCardinalitiesV(Lp.size());
+    for (auto& tmp : setCoverTable_s) {
+        for (int i = 0;i < tmp.size();++i) {
+            setsCardinalitiesV[i] += tmp[i];
+        }
+    }
+    vector<int> setsCardinalitiesL(vlen);
+    for (int i = 0;i < vlen;++i) {
+        setsCardinalitiesL[i] = std::accumulate(setCoverTable_s[i].begin(), setCoverTable_s[i].end(), 0);
+    }
+    auto& A = setCoverTable_s;
+    auto& setsLabelsV = Lp;
+    auto solution_setsLabelsV = SetCoveringProblem(A, setsLabelsV, setsCardinalitiesV, setsCardinalitiesL);
+
     return 0;
+}
+
+vector<int> SetCoveringProblem(vector<vector<bool>>& A, vector<int>& setsLabelsV, vector<int>& setsCardinalitiesV, vector<int>& setsCardinalitiesL) {
+    //Identify elements covered by just one set and select them
+    vector<bool> UniquelyCoveredL(setsCardinalitiesL.size());
+    for (int i = 0;i < setsCardinalitiesL.size();++i) {
+        UniquelyCoveredL[i] = (setsCardinalitiesL[i] == 1);
+    }
+    if (std::accumulate(UniquelyCoveredL.begin(), UniquelyCoveredL.end(), 0) > 0) {
+        //TODO
+    }
 }
